6612b6e1b4de087783e0635f39b00db159399972
Your Name <you@example.com>
2024-10-08 17:56:20 +0530

    update

---
+++ constant/connect.php
@@ -0,0 +1,15 @@
+<?php 
+// DB credentials.
+$localhost = "localhost";
+$username = "root";
+$password = "musab";
+$dbname = "youthappam";
+// db connection
+$connect = new mysqli($localhost, $username, $password, $dbname);
+// check connection
+if($connect->connect_error) {
+  die("Connection Failed : " . $connect->connect_error);
+} else {
+  //echo "Successfully connected";
+}
+?>
